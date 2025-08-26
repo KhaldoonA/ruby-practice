0b58b1260463c846cb851077e25317d165cf736f
KhaldoonA <kabourahma1@gmail.com>
2025-08-26 01:46:06 -0500

    Update temp_converter.rb

--- temp_converter.rb
+++ temp_converter.rb
@@ -1,26 +1,27 @@
 puts "Would you like to convert to Celsius or Fahrenheit? Please enter C for Celsius or F for Fahrenheit"
-type = gets.chomp
+type = gets.chomp.upcase
 
 def fah
   puts "Please enter a temperature in Celsius:"
-  temp_c = gets
-  temp_f = temp_c.to_i * (9.0/5) + 32
-  puts temp_f
+  temp_c = gets.chomp.to_f
+  temp_f = temp_c * (9.0/5) + 32
+  puts "The converted temperature is now #{temp_f}"
 end
 
 def cel
   puts "Please enter a temperature in Fahrenheit:"
-  temp_f = gets
-  temp_c = (temp_f.to_i - 32) * (5.0/9)
-  puts temp_c
+  temp_f = gets.chomp.to_f
+  temp_c = (temp_f - 32) * (5.0/9)
+  puts "The converted temperature is now #{temp_c}"
 end
 
-if type == "F"
+if type.upcase == "F"
   fah
-elsif type == "C"
+elsif type.upcase == "C"
   cel
 else  
   puts "Invalid Choice!"
 end
+
 
 
